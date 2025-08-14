62f092e88f9419354e19964375f2fa973a8d2b0c
kaylegalanto23 <kylegalanto23@gmail.com>
2025-08-14 13:30:05 +0800

    GitUserInputAct4

---
+++ act4.dart
@@ -0,0 +1,23 @@
+import 'dart:io';
+
+void main() {
+    print(" Enter your name");
+    String? name = stdin.readLineSync();
+    print("your name is ${name}");
+
+    print(" Enter your section");
+    String? section = stdin.readLineSync();
+    print("your section is ${section}");
+
+    print(" Enter your course");
+    String? course = stdin.readLineSync();
+    print("your course is ${course}");
+
+    print(" Enter your yearlevel");
+    String? yearlevel = stdin.readLineSync();
+    print("your yearlevel is ${yearlevel}");
+
+    print(" Enter your subject");
+    String? subject = stdin.readLineSync();
+    print("your name is ${subject}");
+}
